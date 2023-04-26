031f2149430ef952d152245cf6d953cab9e3d183
Sunday Onwuchekwa <donsonde@gmail.com>
2023-04-26 19:02:42 +0000

    New website

---
+++ Web Development Project/mail/contact_me.php
@@ -0,0 +1,21 @@
+<?php
+    // Check for empty fileds
+    if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        echo "No argument provided!";
+        return false;
+    }
+
+    $name = $_POST['name'];
+    $email_address = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+
+    // Create the email and send the message
+    $to = "etiannejohnson@gmail.com"; // Add your email
+    $email_subject = "Website Contact Form: $name";
+    $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage: \n$message";
+    $headers = "From: noreply@etiannejohnson.com\n";
+    $headers .= "Reply-To: $email_address";
+    mail($to, $email_subject, $email_body, $headers);
+    return true;
+?>
